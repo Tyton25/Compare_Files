e93b2fcb69007aa23241f615eeb60ab95d571c5e
Tyton25 <Tyton25@users.noreply.github.com>
2017-07-15 00:44:11 -0600

    Update Compare_Files.py

--- Compare_Files.py
+++ Compare_Files.py
@@ -7,6 +7,17 @@ def main():
   t_file_dir = ''
   
   p = parseArguments()
-
+  
+  p.addOption('-o', '--output',
+              type='string',
+              source='outfile',
+              nargs=1,
+              help='Specifies the output file name.')
+  p.addOption('-t', '--txt',
+              type='string',
+              source='txtfile',
+              nargs=1,
+              help='Specifies the file with which other files are compared.')
+  
 if __NAME__ == '__MAIN__':
     main()
