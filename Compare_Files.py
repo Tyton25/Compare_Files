869eb9ae37f603c0fc5e99addfcaa38caf26adaa
Tyton25 <Tyton25@users.noreply.github.com>
2017-07-15 01:26:16 -0600

    Update Compare_Files.py

--- Compare_Files.py
+++ Compare_Files.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python
 
+from optparse import OptionParser
+import realine, os, sys
 
 def main():
   t_file = ''
-  t_file_input = ''
-  t_file_dir = ''
+  t_file_dir = os.getenv('PWD')
   
-  p = parseArguments()
   
-  p.addOption('-o', '--output',
+  p = OptionParser(usage: '%prog -t <input_file1.txt> <input_file2.txt>')
+  
+  p.add_option('-o', '--output',
               type='string',
               action='store',
               dest='outfile',
               nargs=1,
               help='Specifies the output file name.')
-  p.addOption('-t', '--txt',
+  p.add_option('-t', '--txt',
               type='string',
               action='store',
               dest='txtfile',
               nargs=1,
               help='Specifies the file with which other files are compared.')
+  (args, opts) = p.parse_args()
   
+  if opts.txtfile:
+    t_file = opts.txtfile
+    
+def compare_files(txtfile,outfile,args_length):
+  results_file = ''
+  infile = ''
+  
+  for line in input.readlines(txtfile):
+    infile = infile + line + "\n"
+  
+  return results_file
+
 if __NAME__ == '__MAIN__':
     main()
